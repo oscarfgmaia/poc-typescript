1b73f7f01b6ec43b0d7040f766c1bfb6d47877a2
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-22 22:18:52 -0300

    feat: register payment controller

--- src/controllers/payment-controller.ts
+++ src/controllers/payment-controller.ts
@@ -1,14 +1,19 @@
 import { Payment } from "../protocols.js";
 import { Request, Response } from "express";
 import paymentsRepository from "../repositories/payment-repository.js";
+import { paymentSchema } from "../schemas/paymentSchema.js";
 
 export async function registerPayment(req: Request, res: Response) {
-  const payment = req.body;
-  console.log(payment)
+  const payment = req.body as Payment;
+  const { error } = paymentSchema.validate(payment,{abortEarly:false});
+  if (error) {
+    const errors = error.details.map((detail) => detail.message);
+    return res.status(400).send(errors);
+  }
   try {
     await paymentsRepository.registerPayment(payment);
     res.sendStatus(201);
   } catch (error) {
-    console.log(error)
+    res.sendStatus(500);
   }
 }
