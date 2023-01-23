cac77cd1fb9f1cdc14e0de71584eb2a8ed2fc477
Oscar Maia <oscarfgmaia@gmail.com>
2023-01-22 23:02:41 -0300

    feat: create update delete and get payments

--- src/controllers/payment-controller.ts
+++ src/controllers/payment-controller.ts
@@ -2,18 +2,55 @@ import { Payment } from "../protocols.js";
 import { Request, Response } from "express";
 import paymentsRepository from "../repositories/payment-repository.js";
 import { paymentSchema } from "../schemas/paymentSchema.js";
+import httpStatus from "http-status";
 
 export async function registerPayment(req: Request, res: Response) {
   const payment = req.body as Payment;
-  const { error } = paymentSchema.validate(payment,{abortEarly:false});
+  const { error } = paymentSchema.validate(payment, { abortEarly: false });
   if (error) {
     const errors = error.details.map((detail) => detail.message);
     return res.status(400).send(errors);
   }
   try {
     await paymentsRepository.registerPayment(payment);
-    res.sendStatus(201);
+    res.sendStatus(httpStatus.CREATED);
   } catch (error) {
     res.sendStatus(500);
+  }
+}
+
+export async function getPayments(req: Request, res: Response) {
+  try {
+    const payments = await paymentsRepository.getAllPayments();
+    if (payments.rowCount === 0) {
+      return res.sendStatus(httpStatus.NO_CONTENT);
+    } else {
+      return res.send(payments.rows);
+    }
+  } catch (error) {
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function deletePayment(req: Request, res: Response) {
+  const { id } = req.params;
+  try {
+    await paymentsRepository.deletePayment(id);
+    res.sendStatus(httpStatus.OK);
+  } catch (error) {
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
+  }
+}
+
+export async function updatePayment(req: Request, res: Response) {
+  const payment = req.body as Payment;
+  const { id } = req.params;
+  console.log(payment)
+  console.log(id)
+  try {
+    await paymentsRepository.updatePayment(payment, id);
+    res.sendStatus(httpStatus.OK);
+  } catch (error) {
+    res.sendStatus(httpStatus.INTERNAL_SERVER_ERROR);
   }
 }
--- src/repositories/payment-repository.ts
+++ src/repositories/payment-repository.ts
@@ -1,3 +1,4 @@
+import { QueryResult } from "pg";
 import connection from "../database/database.js";
 import { Payment } from "../protocols.js";
 
@@ -10,12 +11,53 @@ async function registerPayment(payment: Payment) {
       [payment.value, payment.description]
     );
   } catch (error) {
-    throw error
+    throw error;
+  }
+}
+
+async function getAllPayments(): Promise<QueryResult<Payment>> {
+  try {
+    return await connection.query(`
+        SELECT * FROM payments;
+      `);
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function deletePayment(id: string) {
+  try {
+    await connection.query(
+      `
+          DELETE FROM payments WHERE id=$1;
+        `,
+      [id]
+    );
+  } catch (error) {
+    throw error;
+  }
+}
+
+//TODO FIX UPDATE
+async function updatePayment(payment: Payment, id: string) {
+  try {
+
+    await connection.query(
+      `
+            UPDATE payments SET value=$2, description=$3 WHERE id=$4;
+          `,
+      [payment.value, payment.description, id]
+    );
+  } catch (error) {
+    throw error;
   }
 }
 
 const paymentsRepository = {
   registerPayment,
+  getAllPayments,
+  deletePayment,
+  updatePayment,
 };
 
 export default paymentsRepository;
